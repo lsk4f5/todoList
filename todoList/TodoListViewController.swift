0884901f35dea7a9d9fa5fdf5ce5e758f4fcf023
lsk4f5 <miseri.osk12@gmail.com>
2020-05-18 23:18:21 +0900

    アラートの作成と新規アイテム追加

--- todoList/TodoListViewController.swift
+++ todoList/TodoListViewController.swift
@@ -75,5 +75,34 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
 
     }
+    
+    // MARK - 新規アイテム追加機能
+    @IBAction func addButtonPressed(_ sender: Any) {
+        // プラスボタンが押された時に実行される処理
+
+        var textField = UITextField()
+
+        let alert = UIAlertController(title: "新しいアイテムを追加", message: "", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "リストに追加", style: .default) { (action) in
+            // 「リストに追加」を押された時に実行される処理
+
+            let newItem: Item = Item(title: textField.text!)
+
+            // アイテム追加処理
+            self.itemArray.append(newItem)
+            self.tableView.reloadData()
+
+        }
+
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "新しいアイテム"
+            textField = alertTextField
+        }
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+
+    }
 }
 
