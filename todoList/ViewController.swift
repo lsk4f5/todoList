a16ec0ff6d7bfa8d814ef1747530360024c55ee0
lsk4f5 <miseri.osk12@gmail.com>
2020-05-18 21:46:23 +0900

    Initial Commit

---
+++ todoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoList
+//
+//  Created by osakamiseri on 2020/05/18.
+//  Copyright © 2020 osakamiseri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
